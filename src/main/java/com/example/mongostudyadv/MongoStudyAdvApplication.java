9af10142b1bb1ceb2c45adeca70311c75e0cf55c
YUN <yunmyeonghun@gmail.com>
2021-08-19 18:37:18 +0900

    Initial commit

---
+++ src/main/java/com/example/mongostudyadv/MongoStudyAdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongostudyadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoStudyAdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoStudyAdvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongostudyadv/MongoStudyAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongostudyadv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoStudyAdvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
